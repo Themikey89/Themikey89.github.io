34bdc8c03b28a211306e76dc966335de38e0899c
Themikey89 <Elizalde.13@hotmail.com>
2017-01-12 00:48:15 -0600

    edit code

--- send_form_email.php
+++ send_form_email.php
@@ -97,44 +97,16 @@ if(isset($_POST['email'])) {
     died($error_message);
  
   }
+  
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $last_name);
+  fwrite($myfile, $first_name);
+  fwrite($myfile, $email_form);
+  fwrite($myfile, $comments);
+  fwrite($myfile, $telephone);
+  fclose($myfile);
  
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
+// create text 
  
 ?>
  
